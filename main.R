6514fbb7cb6346e8da42f186a5cdd94231314ee9
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-11-30 11:24:47 +0300

    pdf merger code

---
+++ main.R
@@ -0,0 +1,22 @@
+
+# Load the pdftools package
+library(pdftools)
+
+# Define the directory containing PDF files
+pdf_directory <- "D:/My Docs"
+
+# Manually specify the file names in the desired order
+pdf_files <- c("CV_JEREMIAH_KATUMO_KURWA.pdf","SCANNED_CERT.pdf", "JEREMIAH_KCSE_CERT.pdf", "National Association of State Boards of Accountancy (NASBA).pdf", "MEAL DPro_Certificate of completion.pdf",
+               "Survival Analysis in R.pdf", "Factor Analysis in R.pdf", "Experimental Design in R.pdf", "Analyzing Survey Data in R.pdf", "Intro to machine learning with R.pdf",
+               "Machine_Learning.pdf", "Tableau Training.pdf", "Power_BI_for_Beginners.pdf", "Business analytics with Excel.pdf", "Transcript.pdf")  # Replace with your filenames
+
+# Construct the full file paths
+pdf_files <- file.path(pdf_directory, pdf_files)
+
+# Specify the output file
+output_file <- file.path(pdf_directory, "merged_output.pdf")
+
+# Merge the PDF files in the specified order
+pdf_combine(pdf_files, output_file)
+
+cat("PDF files merged successfully in the specified order into", output_file, "\n")
